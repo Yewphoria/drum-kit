e3b2a72312d15079c7a439d3067a4e706fc8719e
Yewphoria <122328211+Yewphoria@users.noreply.github.com>
2024-07-27 21:38:45 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,90 @@
+
+
+// document.querySelector("button").addEventListener("click",handleClick);
+
+// function handleClick(){
+//     alert("I got clicked!");
+// }
+
+// Another way of writing this 
+
+// document.querySelector("button").addEventListener("click",function () {
+//     alert("I got clicked!");
+//     //what to do
+// }
+// );
+
+
+// adding eventListener to all drum button
+
+var noDrumButton = document.querySelectorAll(".drum").length;
+//detect button
+for(var i=0; i<noDrumButton;i++){   //better to use drum class to be more specific 
+    document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+       })
+}
+//detect keyboard
+document.addEventListener("keydown",function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+    });
+
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+       tom1.play();
+       break;
+
+       case "a" :
+        var tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+        case "s":
+        var tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+        case "d": 
+        var tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+        case "j": 
+        var crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+        break;
+
+        case "k": 
+        var kick = new Audio("./sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+        case "l": 
+        var snare = new Audio("./sounds/snare.mp3");
+        snare.play();
+        break;
+
+
+        default: console.log(buttonInnerHTML);
+}
+}
+
+function buttonAnimation(key){
+  var activeButton= document.querySelector("."+key) //class selector
+  activeButton.classList.add("pressed");
+  //delay function to remove the "pressed" class
+    setTimeout(function(){
+    activeButton.classList.remove("pressed");
+   },100);
+}
+
+
+// var audio = new Audio("./sounds/tom-1.mp3");
+//         audio.play();
